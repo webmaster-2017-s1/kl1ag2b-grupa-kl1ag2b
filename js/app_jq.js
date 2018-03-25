5e283c155fb9c1dbf637056920c7115f37f6cc3f
rawskim <michalrawski01@gmail.com>
2018-03-25 16:05:03 +0200

    wstawienie nazw kart do js

--- js/app_jq.js
+++ js/app_jq.js
@@ -1,4 +1,1 @@
-// kod projektu [memory]
-$(document).ready(function(){
-  ;
-    });
+var cards = ["o1.png", "o2.png", "o3.png", "o4.png", "o5.png", "o6.png", "o7.png", "o8.png", "o9.png", "o10.png"];
