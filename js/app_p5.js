98b98e58a1f0363654519d86215af51d4415bde2
dzbanek7 <adamgarnuszek@gmail.com>
2018-03-27 19:24:12 +0200

    wyśrodkowanie planszy

--- js/app_p5.js
+++ js/app_p5.js
@@ -1,9 +1,10 @@
 
 function setup() { //funkcja opisująca czynności wykonujące się zaraz po uruchomieniu strony
-  createCanvas(320, 500); // "stwórz krawedzie" - 320px x 500px
+  createCanvas(320, 320); // "stwórz krawedzie" - 320px x 500px
   background("#fff");
 
 }
+//
 
 function draw() {
 
