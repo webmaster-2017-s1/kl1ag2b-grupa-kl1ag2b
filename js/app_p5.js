c40dde0e9f244f550b46d4451059a05ca15b438b
dzbanek7 <adamgarnuszek@gmail.com>
2018-03-27 18:24:53 +0200

    dodanie funkcji setup

--- js/app_p5.js
+++ js/app_p5.js
@@ -1,9 +1,10 @@
-// kod projektu [memory]
-function setup() {
-  // put setup code here
+
+function setup() { //funkcja opisująca czynności wykonujące się zaraz po uruchomieniu strony
+  createCanvas(320, 500); // "stwórz krawedzie" - 320px x 500px
+  background("#fff");
+
 }
 
 function draw() {
-  // put drawing code here
 
-}
+  }
